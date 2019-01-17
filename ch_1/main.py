daea9a912a6407572081267909ed619f1409f9a4
Arun Singh <arunsingh.in@gmail.com>
2019-01-17 12:56:47 +0530

    First assignment

---
+++ ch_1/main.py
@@ -0,0 +1,44 @@
+'''
+
+@Author : Arun Singh, arunsingh.in@gmail.com, Pirple.com Python is Easy Assignment
+
+Details:
+ 
+What's your favorite song?
+
+Think of all the attributes that you could use to describe that song. 
+That is: all of it's details or "meta-data". These are attributes like "Artist", "Year Released", "Genre", "Duration", etc. Think of as many 
+different characteristics as you can.
+
+In your text editor, create an empty file and name it main.py
+
+Now, within that file, list all of the attributes of the song, one after another, by creating variables for each attribute, 
+and giving each variable a value. Here's an example:
+
+
+'''
+
+
+title  = "Kaho Na Pyaar hai"
+artist = "Lucky Ali, Ramya"
+genre = "romantic"
+lyrics = "gulzar"
+Durationinseconds = "302seconds"
+language = "hindi"
+popularity = "***"  # 3 star rating and review out of 5 total stars
+Released = "2000"  # number will be considered as string since its between the double quotes.
+distributionRights = "Venus Worldwide Entertainment Pvt Ltd"
+
+
+
+print(title)
+print(artist)
+print(genre)
+print(lyrics)
+print(Durationinseconds)
+print(language)
+print(popularity)
+print(Released)
+print(distributionRights)
+
+
